49bb270a9376296bc41b8de0dc0c820e7be672f6
姜威 <1468222675@qq.com>
2019-03-13 15:38:20 +0800

    create repository

---
+++ src/main/java/com/yiban/springboot_soul/SpringbootSoulApplication.java
@@ -0,0 +1,13 @@
+package com.yiban.springboot_soul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootSoulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootSoulApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/yiban/springboot_soul/controller/HelloController.java
@@ -0,0 +1,21 @@
+package com.yiban.springboot_soul.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+
+    @ResponseBody
+    @RequestMapping("/login")
+    public String hello(){
+        return "Hello World";
+    }
+
+    @RequestMapping("/success")
+    public String success(){
+        return "success";
+    }
+}
